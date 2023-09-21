7b4fb6bb84f4d4fba69e25009851ced5150bd768
Ahamed Aaqib <aaqib@edsmart.com>
2023-09-21 22:20:59 +0530

    Add QuickViewPopup Comp

---
+++ PieShopHRM/Components/QuickViewPopup.razor.cs
@@ -0,0 +1,24 @@
+﻿using BethanysPieShopHRM.Shared.Domain;
+using Microsoft.AspNetCore.Components;
+
+namespace PieShopHRM.Components
+{
+    public partial class QuickViewPopup
+    {
+        [Parameter]
+        public Employee? SelectedEmployee { get; set; }
+
+        private  Employee? _employee;
+
+        protected override void OnParametersSet()
+        {
+            _employee = SelectedEmployee;
+        }
+
+        public void Close()
+        {
+            _employee = null;
+        }
+
+    }
+}
