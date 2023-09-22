65dd251ff46206eb7f7208621b64124421cdae32
Ahamed Aaqib <aaqib@edsmart.com>
2023-09-22 17:07:24 +0530

    Add EmployeeDetails

---
+++ PieShopHRM/Pages/EmployeeDetails.razor.cs
@@ -0,0 +1,21 @@
+﻿using BethanysPieShopHRM.Shared.Domain;
+using Microsoft.AspNetCore.Components;
+using PieShopHRM.Models;
+
+namespace PieShopHRM.Pages
+{
+    public partial class EmployeeDetails
+    {
+        public Employee? Employee { get; set; } = new Employee();
+
+        [Parameter]
+        public string EmployeeId { get; set; } = string.Empty;
+
+        protected override Task OnInitializedAsync()
+        {
+            Employee = MockDataService.Employees.FirstOrDefault(e => e.EmployeeId == int.Parse(EmployeeId));
+
+            return base.OnInitializedAsync();
+        }
+    }
+}
