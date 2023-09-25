639335e080c874d948fa2b0218c3d929aa7f89d7
Ahamed Aaqib <aaqib@edsmart.com>
2023-09-25 10:47:48 +0530

    Add Widgets using DynamicComponent

---
+++ PieShopHRM/Pages/Index.razor.cs
@@ -0,0 +1,9 @@
+﻿using PieShopHRM.Components.Widgets;
+
+namespace PieShopHRM.Pages
+{
+    public partial class Index
+    {
+        List<Type> Widgets { get; set; } = new List<Type>() { typeof (EmployeeCountWidget), typeof(InboxWidget)};
+    }
+}
