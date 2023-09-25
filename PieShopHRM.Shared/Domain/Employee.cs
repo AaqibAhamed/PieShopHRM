ad81a893698c8184c4d555aa8bb71133d6a64d4f
Ahamed Aaqib <aaqib@edsmart.com>
2023-09-25 15:03:04 +0530

    Update Entire Domain  namespace

--- PieShopHRM.Shared/Domain/Employee.cs
+++ PieShopHRM.Shared/Domain/Employee.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-namespace BethanysPieShopHRM.Shared.Domain
+namespace PieShopHRM.Shared.Domain
 {
     public class Employee
     {
--- PieShopHRM/Pages/EmployeeOverview.razor.cs
+++ PieShopHRM/Pages/EmployeeOverview.razor.cs
@@ -1,4 +1,4 @@
-﻿using BethanysPieShopHRM.Shared.Domain;
+﻿using PieShopHRM.Shared.Domain;
 using PieShopHRM.Models;
 
 namespace PieShopHRM.Pages
