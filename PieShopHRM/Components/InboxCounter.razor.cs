b62950afffb92fa0656caf47064784bf5c340160
Ahamed Aaqib <aaqib@edsmart.com>
2023-09-26 15:57:45 +0530

    Add InboxCounter Comp

---
+++ PieShopHRM/Components/InboxCounter.razor.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace PieShopHRM.Components
+{
+    public partial class InboxCounter
+    {
+        private int MessageCount;
+
+        [Inject]
+        public AppState? AppState { get; set; }
+
+        protected override void OnInitialized()
+        {
+            MessageCount = new Random().Next(10);
+            AppState.NumberOfMessages = MessageCount;
+        }
+    }
+}
