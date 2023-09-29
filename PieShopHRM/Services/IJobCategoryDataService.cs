c0f2912478ea52e6199cd1a6c2b32cdc898ee741
Ahamed Aaqib <aaqib@edsmart.com>
2023-09-29 11:54:23 +0530

    Add JobCategoryDataService

---
+++ PieShopHRM/Services/IJobCategoryDataService.cs
@@ -0,0 +1,10 @@
+﻿using PieShopHRM.Shared.Domain;
+
+namespace PieShopHRM.Services
+{
+    public interface IJobCategoryDataService
+    {
+        Task<IEnumerable<JobCategory>> GetAllJobCategories();
+        Task<JobCategory> GetJobCategoryById(int jobCategoryId);
+    }
+}
---
+++ PieShopHRM/Services/JobCategoryDataService.cs
@@ -0,0 +1,28 @@
+﻿using PieShopHRM.Shared.Domain;
+using System.Net.Http;
+using System.Text.Json;
+
+namespace PieShopHRM.Services
+{
+    public class JobCategoryDataService : IJobCategoryDataService
+    {
+        private HttpClient _httpClient;
+        public JobCategoryDataService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+        public async Task<IEnumerable<JobCategory>> GetAllJobCategories()
+        {
+            return await JsonSerializer.DeserializeAsync<IEnumerable<JobCategory>>
+              (await _httpClient.GetStreamAsync($"api/jobCategory"),
+              new JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
+        }
+
+        public async Task<JobCategory> GetJobCategoryById(int jobCategoryId)
+        {
+            return await JsonSerializer.DeserializeAsync<JobCategory>
+             (await _httpClient.GetStreamAsync($"api/jobcategory/{jobCategoryId}"),
+             new JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
+        }
+    }
+}
