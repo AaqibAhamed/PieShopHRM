38bf0c2d52bed29f792f91d4c03a060386ed8be9
Ahamed Aaqib <aaqib@edsmart.com>
2023-09-30 17:38:56 +0530

    Add Map

--- PieShopHRM/Pages/EmployeeDetails.razor.cs
+++ PieShopHRM/Pages/EmployeeDetails.razor.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Components;
 using PieShopHRM.Models;
 using PieShopHRM.Services;
+using PieShopHRM.Shared.Model;
 
 namespace PieShopHRM.Pages
 {
@@ -15,13 +16,29 @@ namespace PieShopHRM.Pages
         [Parameter]
         public string EmployeeId { get; set; } = string.Empty;
 
+        public List<Marker> MapMarkers = new();
+
         protected override async Task OnInitializedAsync()
         {
             // Employee = MockDataService.Employees.FirstOrDefault(e => e.EmployeeId == int.Parse(EmployeeId));
 
             Employee = await EmployeeDataService.GetEmployeeDetails(int.Parse(EmployeeId));
 
-            
+            if (Employee.Latitude.HasValue && Employee.Longitude.HasValue)
+            {
+                MapMarkers = new List<Marker>
+                {
+                    new Marker
+                    {
+                         Description = $"{Employee.FirstName} {Employee.LastName}",
+                         ShowPopup = false ,
+                         X= Employee.Latitude.Value,
+                         Y= Employee.Longitude.Value
+                    }
+                };
+            }
+
+
         }
     }
 }
