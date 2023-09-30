8ae8735a0853790659dde2d24defd8dc1b25813a
Ahamed Aaqib <aaqib@edsmart.com>
2023-09-30 17:37:48 +0530

    Add PieShopHRM.ComponentsLibrary

---
+++ PieShopHRM.ComponentsLibrary/Map.razor.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using PieShopHRM.Shared.Model;
+
+namespace PieShopHRM.ComponentsLibrary
+{
+    public partial class Map
+    {
+        [Inject]
+        public IJSRuntime JSRuntime { get; set; }
+
+        [Parameter]
+        public double Zoom { get; set; }
+
+        [Parameter] 
+        public List<Marker> Markers { get; set; }
+
+        string elementId = $"map-{Guid.NewGuid():D}";
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            await JSRuntime.InvokeVoidAsync("deliveryMap.showOrUpdate", elementId, Markers);
+        }
+
+
+    }
+}
